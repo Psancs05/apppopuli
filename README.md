073f0598c4a81bceb20484f46c56fc7250c9f263
javier <psancs05@estudiantes.unileon.es>
2024-05-22 19:40:27 +0200

    Updated IA service

--- README.md
+++ README.md
@@ -1,24 +1,35 @@
 # app-populi-vtfy
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+# Env vars
+
+Include a `.env` file with the following env variables:
+
+- API_URL (str): Modal API url
--- src/services/IAService.js
+++ src/services/IAService.js
@@ -1,22 +1,46 @@
 import axios from 'axios';
+require('dotenv').config();
 
-const API_URL = 'https://cairns-bilby-rczk.1.ie-1.fl0.io/api';
+const API_URL = process.env.API_URL;
 
 class IAService {
-  analizarImagen(imageFile) {
-    const formData = new FormData();
-    formData.append('image', imageFile);
-    formData.append('ruta_model', "./data/models/modelo.h5");
+  async analizarImagen(imageFile) {
+    try {
+      // Convertir la imagen a base64
+      const mybase64 = await this.convertFileToBase64(imageFile);
 
-    const config = {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      }
-    };
+      // Crear el objeto de datos a enviar
+      const data = {
+        image: mybase64,
+      };
 
-    return axios.post(API_URL, formData, config);
+      // Configurar la solicitud
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+
+      // Enviar la solicitud
+      return axios.post(API_URL, data, config);
+    } catch (error) {
+      console.error('Error al convertir la imagen a base64:', error);
+      throw error;
+    }
   }
 
+  convertFileToBase64(file) {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.onload = () => {
+        resolve(fileReader.result.split(',')[1]); // Obtener solo la parte base64 de la cadena
+      };
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+      fileReader.readAsDataURL(file);
+    });
+  }
 }
 
 export default new IAService();
